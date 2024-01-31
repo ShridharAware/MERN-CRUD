b9222aa3e6ec734239eff3e63e8ebe82f483f72e
Shridhar Aware <130430834+ShridharAware@users.noreply.github.com>
2024-01-31 17:22:49 +0530

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,14 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://crud-backend-lac.vercel.app/', // Replace with your Flask server URL
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
+  plugins: [react()]
 })
