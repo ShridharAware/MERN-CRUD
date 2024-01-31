bf1f7264bc5c75cafa7a4828e826e37b280dc3a4
Shridhar Aware <130430834+ShridharAware@users.noreply.github.com>
2024-01-31 17:38:51 +0530

    Update Update.tsx

--- client/src/pages/Update.tsx
+++ client/src/pages/Update.tsx
@@ -25,7 +25,7 @@ const UpdateUser: React.FC = () => {
   const updateUser = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault(); // Prevent the default form submission behavior
     try {
-      const url = `https://crud-backend-lac.vercel.app/user/${id}`;
+      const url = `https://crud-frontend-coral.vercel.app/user/${id}`;
       await axios
         .put(url, formData)
         .then(() => {
