f615cff9b1dd1651fafb46756fa9513fb136bb3b
Shridhar Aware <130430834+ShridharAware@users.noreply.github.com>
2024-01-31 17:48:18 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,7 +14,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="register" element={<Register />} />
           <Route path="user" element={<Users />} />
-          <Route path="/user/:id" element={<Update />} />
+          <Route path="user/:id" element={<Update />} />
         </Routes>
       </Router>
     </>
